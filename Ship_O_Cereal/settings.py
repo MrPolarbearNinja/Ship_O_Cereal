242182c2e21403ce238ad5f9ca268d7d39233548
rognvaldur20 <rognvaldur20@ru.is>
2021-05-04 14:41:24 +0000

    settings uppfært með database stillingum

--- Ship_O_Cereal/settings.py
+++ Ship_O_Cereal/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +77,12 @@ WSGI_APPLICATION = 'Ship_O_Cereal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ship-o-cereal-db',
+        'USER': 'ship-o-cereal-db-user',
+        'PASSWORD': 'shipo',
+        'HOST': '34.105.232.43',
+        'PORT': '5432',
     }
 }
 
@@ -121,7 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
