fb3a52898dd8ddb31eb0f39556b5ffe2e322a23b
Arcmazter <aronlex@gmail.com>
2021-05-04 11:01:34 +0000

    url

--- Ship_O_Cereal/urls.py
+++ Ship_O_Cereal/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('catalogue.urls')),
     path('admin/', admin.site.urls),
     path('ShipOCereal/', include('ShipOCereal.urls')),
     path('catalogue/', include('catalogue.urls')),
