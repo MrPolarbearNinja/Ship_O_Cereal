a30fc16ff482787fe353ed3de31baac0f5104c52
Arcmazter <aronlex@gmail.com>
2021-05-04 16:01:31 +0000

    data stuff

--- product_info/models.py
+++ product_info/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Item_Type(models.Model):
+    name = models.charField(max_length=255)
+
+class Items(models.Model):
+    name = models.charField(max_length=255)
+    description = models.charField(max_length=255, blank=True)
+    price = models.FloatField()
+    type = models.ForeignKey(Item_Type, on_delete=models.CASCADE)
+
+class Item_Stock(models.Model):
+    item = models.ForeignKey(Item_Type, on_delete=models.CASCADE)
+    quantity = models.FloatField()
