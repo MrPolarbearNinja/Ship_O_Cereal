3ae19d1b9407adb5506275dde86408e614e9162c
Arcmazter <aronlex@gmail.com>
2021-05-05 16:12:46 +0000

    Likning data

--- product_info/models.py
+++ product_info/models.py
@@ -14,3 +14,7 @@ class Item_Stock(models.Model):
     item = models.ForeignKey(Item_Type, on_delete=models.CASCADE)
     quantity = models.FloatField()
 
+class Items_Image(models.Model):
+    item = models.ForeignKey(Item_Type, on_delete=models.CASCADE)
+    item_link = models.CharField(max_length=255)
+
