12531a094fe232cdf715b7ee612b2a173e856863
Arcmazter <aronlex@gmail.com>
2021-05-06 13:58:17 +0000

    item details

--- product_info/urls.py
+++ product_info/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     ## http://localhost:8000/catalogue
     path('', views.index, name="index"),
+    path('<int:id>', views.get_item_by_id, name="Product_info")
 ]
--- product_info/views.py
+++ product_info/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from product_info.models import Items
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'product_info/Item_Detail.html')
+    context = {'Items': Items.objects.all().order_by('name')}
+    return render(request, 'catalogue/catalogue.html', context)
+
+def get_item_by_id(request, id):
+    return render(request, 'product_info/Item_Detail.html', {
+        'item' : get_object_or_404(Items, pk=id)
+    })
+
+
 
 
 # Create your views here.
