282e099a89f942bd03814bddbc157700af4b5d6d
Arcmazter <aronlex@gmail.com>
2021-05-06 11:04:20 +0000

    Catalog items

--- product_info/views.py
+++ product_info/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from product_Info')
+    return render(request, 'product_info/Item_Detail.html')
 
-
-from django.shortcuts import render
 
 # Create your views here.
