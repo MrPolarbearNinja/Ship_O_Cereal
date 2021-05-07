3ade8f8e24476d50d05beacb9a557b1579de67c2
Palli95 <palljonsson95@gmail.com>
2021-05-07 10:42:32 +0000

    edit-accountfix

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     ## http://localhost:8000/catalogue
     path('', views.index, name="index"),
     path('create_account', views.create_account, name='create_account'),
-    path('edit_account', views.edit_account, name='create_account')
+    path('edit_account', views.edit_account, name='edit_account')
 ]
