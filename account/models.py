1374aa669f1c7f6b5ef443b6b005487fdfdf129d
Arcmazter <aronlex@gmail.com>
2021-05-07 15:59:04 +0000

    change user model

--- account/models.py
+++ account/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(AbstractUser):
-    username = models.CharField(max_length=255, unique=True )
-    password = models.CharField(max_length=255)
+class User(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     image = models.CharField(max_length=255, blank=True)
+
+
 
 
