0075347ee5093c47a4b9cffe0c557e1dc4b18d20
Palli95 <palljonsson95@gmail.com>
2021-05-08 11:40:32 +0000

    urlsfix

--- account/urls.py
+++ account/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
     ## http://localhost:8000/catalogue
     path('', views.index, name="index"),
+    path('register', views.register, name='register'),
     path('create_account', views.create_account, name='create_account'),
     path('edit_account', views.edit_account, name='edit_account'),
     path('login', LoginView.as_view(template_name='account_log_in/login.html'), name='login'),
     path('logout', LogoutView.as_view(next_page='login'), name='logout'),
-    path('register', views.register, name='Register')
+
 ]
