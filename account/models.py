521af37e90b112f9512df4fb375fbf5a8cd87bc4
Arcmazter <aronlex@gmail.com>
2021-05-10 11:28:33 +0000

    Migrate plz

--- account/models.py
+++ account/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class User(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    image = models.CharField(max_length=255, blank=True)
+    image = models.CharField(max_length=9999, blank=True)
 
 
 
