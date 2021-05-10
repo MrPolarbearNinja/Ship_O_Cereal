ede047abfdf22ef271519865284d7783ea68cdf3
Arcmazter <aronlex@gmail.com>
2021-05-10 17:24:27 +0000

    search more

--- catalogue/views.py
+++ catalogue/views.py
@@ -7,14 +7,9 @@ from django.http import JsonResponse
 def index(request):
     if 'search_filter' in request.GET:
         search_filter = request.GET['search_filter']
-        items = [{
-            'id': x.id,
-            'name': x.name,
-            'description': x.description,
-            'price': x.price,
-            'type_id': x.type_id,
-            'image': x.items_image_set.first().image_link
-        } for x in Items.objects.filter(name__icontains=search_filter)]
-        return JsonResponse({'data': items})
-    context = {'Items' : Items.objects.all().order_by('id')}
+        print(search_filter)
+        context = {'Items': Items.objects.filter(name__contains=search_filter)}
+    else:
+        context = {'Items' : Items.objects.all()
+            .order_by('id')}
     return render(request, 'catalogue/catalogue.html', context)
