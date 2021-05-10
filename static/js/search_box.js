f59f1e22406e9009e5ff1d51726458f76e926f8c
Arcmazter <aronlex@gmail.com>
2021-05-10 18:25:49 +0000

    querry types, not working as of now

--- static/js/search_box.js
+++ static/js/search_box.js
@@ -1,6 +1,23 @@
 
 function search_items(){
     var query = document.getElementById("search-box").value
-    window.location.href = "/?search_filter=" + query
+    var type = ''
+    var radios = document.getElementsByName('type-radio');
 
+    for (var i = 0, length = radios.length; i < length; i++) {
+      if (radios[i].checked) {
+          type = radios[i].value
+          break;
+      }
+    }
+
+    var my_url = ''
+
+    if (query != '')
+        my_url += "/?search_type=" + query
+        if (type != '')
+            my_url += "&?search_type=" + query
+    else if (query == '' && type != '')
+        my_url += "/?search_type=" + query
+    window.location.href = "/?search_filter=" + query
   }
