30205b5e72e63da3b4bc1cb38114693a7349d6b9
Arnór Ingi <arnorg20@ru.is>
2021-05-10 17:24:11 +0000

    tweaks

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     ## http://localhost:8000/catalogue
     path('', views.index, name="index"),
     path('register', views.register, name='register'),
-    path('create_account', views.create_account, name='create_account'),
     path('profile', views.Profile, name='profile'),
     path('login', LoginView.as_view(template_name='account_log_in/login.html'), name='login'),
     path('logout', LogoutView.as_view(next_page='login'), name='logout'),
--- account/views.py
+++ account/views.py
@@ -6,19 +6,7 @@ from account.forms.account_form import Account_Create_Form
 
 def index(request):
     form = Account_Create_Form()
-    return render(request, 'account_log_in/acount_login.html', {
-        'form': form
-    })
-
-def create_account(request):
-    if request.method == 'POST':
-        form = Account_Create_Form(data=request.POST)
-        if form.is_valid():
-            user = form.save()
-            return render(request, 'account_log_in/acount_login.html')
-    else:
-        form = Account_Create_Form()
-    return render(request, 'account_log_in/acount_login.html', {
+    return render(request, 'account_log_in/login.html', {
         'form': form
     })
 
