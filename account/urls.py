6454dc2f82acd6792072cbe270d2bc69281ee04a
Arnór Ingi <arnorg20@ru.is>
2021-05-10 10:38:31 +0000

    changes

--- account/urls.py
+++ account/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     ## http://localhost:8000/catalogue
     path('', views.index, name="index"),
-    path('create-account', views.create_account, name='create_account'),
-    path('edit-account', views.edit_account, name='edit_account'),
-    path('login', LoginView.as_view(template_name='account_log_in/acount_login.html'), name='login'),
-    path('logout', LogoutView.as_view(next_page='login'), name='logout'), #FIXME
-    # path('signup', views.create_account, name='create_account')
+    path('register', views.register, name='register'),
+    path('create_account', views.create_account, name='create_account'),
+    path('edit_account', views.edit_account, name='edit_account'),
+    path('login', LoginView.as_view(template_name='account_log_in/login.html'), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
 ]
