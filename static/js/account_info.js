61c89b7f46c89cab7e1063d4778e2c6bb58f20a8
Arnór Ingi <arnorg20@ru.is>
2021-05-12 14:30:35 +0000

    edit profile /w js

---
+++ static/js/account_info.js
@@ -0,0 +1,25 @@
+
+window.onload = function() {
+    if (window.location.pathname === "/account/profile") {
+        display_save_msg();
+    }
+}
+
+function display_save_msg() {
+    var load = localStorage.getItem("load")
+    if (load == "true") {
+        let resetAttr = () => {
+            document.getElementById("myacc-success").style.display = "none";
+        }
+        setTimeout(resetAttr, 4000);
+        document.getElementById("myacc-success").style.display = "inline-flex";
+    }
+    load = "false";
+    localStorage.setItem("load", load);
+}
+
+function save_changes_acc() {
+    /* setItem only takes in strings */
+    var load = "true";
+    localStorage.setItem("load", load);
+}
