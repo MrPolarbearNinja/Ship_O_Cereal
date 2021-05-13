06f7f7f1962da233a38979bc5699de84f51f99f5
Arcmazter <aronlex@gmail.com>
2021-05-13 10:28:42 +0000

    search

--- basket/urls.py
+++ basket/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     ## http://localhost:8000/catalogue
     path('', views.index, name="index"),
     path('delete_from_basket/<int:id>', views.delete_from_basket, name="delete_from_basket"),
-
 ]
